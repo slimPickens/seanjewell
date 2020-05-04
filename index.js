08ecebae8ca16cd590baa832b81e976981b3391a
slimPickens <sean@seanjewell.com>
2020-05-03 23:55:06 -0400

    test video player

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3030;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
 app.get('/', (req, res) => res.send('Welcome to SeanJewell.com'));
+
+app.get('/live', (req, res) => res.render('live'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
